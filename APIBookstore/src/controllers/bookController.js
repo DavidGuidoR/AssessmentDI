94e60e6c6571319d4595548526ac98207fb7f0f7
DavidGuidoR <GuidoD890@gmail.com>
2025-01-16 16:45:11 -0600

    feat(api): Adicion del endpoint DELETE /books/ el cual permite la eliminación de libros si es un usuario autenticado el que realiza la solicitud, esto concluye la finalización de los requisitos minimos de la API, faltarian extras como la validación del tipo de cada campo explicito mediante express-authenticator o librerias similares, asi mismo una documentación explicita de la API

--- APIBookstore/src/controllers/bookController.js
+++ APIBookstore/src/controllers/bookController.js
@@ -47,6 +47,26 @@ try {
     return res.status(500).json({ error: 'No se pudo registrar el libro.', detalles: error.message });
 }
 };
+
+// Elimination of a book only by an authenticated user
+export const deleteBook = async (req, res) => {
+    const { id } = req.params;
+  
+    try {
+      const book = await db.Book.findByPk(id);
+  
+      if (!book) {
+        return res.status(404).json({ error: 'El libro no fue encontrado.' });
+      }
+  
+      await book.destroy();
+  
+      return res.status(200).json({ success: true });
+    } catch (error) {
+      return res.status(500).json({ error: 'No se pudo eliminar el libro.', detalles: error.message });
+    }
+  };
+  
   
 
 
--- APIBookstore/src/routes/bookRoutes.js
+++ APIBookstore/src/routes/bookRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getAllBooks, createBook} from '../controllers/bookController.js';
+import { getAllBooks, createBook, deleteBook} from '../controllers/bookController.js';
 import { authenticate } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.get('/', getAllBooks);
 
 // requires authentication from the user
 router.post('/', authenticate, createBook )
+router.delete('/:id', authenticate, deleteBook)
 
 export default router;
