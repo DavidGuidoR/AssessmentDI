ac429fe504d35b77c9b3f77f10b02fbf2328a093
DavidGuidoR <GuidoD890@gmail.com>
2025-01-16 15:31:04 -0600

    feat(api): Adición de metodo POST /auth/login/ para logueo de usuarios y generación de token, testeado un par de veces con usuarios existentes y comprobación del token en https://jwt.io

--- APIBookstore/src/controllers/authController.js
+++ APIBookstore/src/controllers/authController.js
@@ -1,5 +1,6 @@
 import bcrypt from 'bcryptjs';
 import db from '../models/index.js';
+import jwt from 'jsonwebtoken';
 
 // User registration in DB
 export const registerUser = async (req, res) => {
@@ -21,3 +22,34 @@ export const registerUser = async (req, res) => {
     return res.status(500).json({ error: 'No se pudo registrar el usuario.', detalles: error.message });
   }
 };
+
+// User login by email and password and generation of a JWT token that expires in 24h
+export const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+  
+    if (!email || !password) {
+      return res.status(400).json({ error: 'El correo y la contraseña son obligatorios.' });
+    }
+  
+    try {
+      const user = await db.User.findOne({ where: { email } });
+  
+      if (!user) {
+        return res.status(404).json({ error: 'El usuario no fue encontrado.' });
+      }
+  
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (!isPasswordValid) {
+        return res.status(401).json({ error: 'Credenciales inválidas.' });
+      }
+      
+      // JWT sign by 24h
+      const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET, {
+        expiresIn: '24h',
+      });
+  
+      return res.status(200).json({ success: true, token });
+    } catch (error) {
+      return res.status(500).json({ error: 'No se pudo iniciar sesión.', detalles: error.message });
+    }
+  };
