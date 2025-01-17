036b6a0b70ab2d80c0b4bc33cc19392d4067418f
DavidGuidoR <GuidoD890@gmail.com>
2025-01-16 18:23:21 -0600

    feat(api): Modificación de las migraciones a CommonJS para funcionamiento con Sequelize, Adicion de validaciones mediante express-validator para los POST de Books Y de Registro de usuario

--- APIBookstore/src/routes/authRoutes.js
+++ APIBookstore/src/routes/authRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { loginUser, registerUser} from '../controllers/authController.js';
+import { createUserValidation } from '../validations/userValidations.js';
+import { validateFields } from '../middlewares/validationMiddleware.js';
 
 const router = express.Router();
 
-router.post('/register', registerUser);
+router.post('/register', createUserValidation, validateFields, registerUser);
 router.post('/login', loginUser);
 
 export default router;
--- APIBookstore/src/routes/bookRoutes.js
+++ APIBookstore/src/routes/bookRoutes.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import { getAllBooks, createBook, deleteBook} from '../controllers/bookController.js';
 import { authenticate } from '../middlewares/authMiddleware.js';
+import { createBookValidation } from '../validations/bookValidations.js';
+import { validateFields } from '../middlewares/validationMiddleware.js';
 
 const router = express.Router();
 
 router.get('/', getAllBooks);
 
 // requires authentication from the user
-router.post('/', authenticate, createBook )
+router.post('/', authenticate, createBookValidation, validateFields, createBook )
 router.delete('/:id', authenticate, deleteBook)
 
 export default router;
